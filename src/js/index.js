ea3bbcbdd9f72d0c78987e083dec2037fb2618c2
Luiz Miguel <108591781+LuizSivl@users.noreply.github.com>
2024-01-30 16:07:41 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+const botao = document.querySelector('.btn-plataforma');
+console.log(botao);
+
+const elementoplataformas = document.querySelector('.btn-plataforma .plataformas');
+
+addEventListener('click',() => {
+    console.log()
+})
